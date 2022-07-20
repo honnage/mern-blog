62a77108a9369e63a9cbfa1cbb7db9bc17bbd7ce
honnage <hon_da-1998@hotmail.com>
2022-07-20 14:36:17 +0700

    test

--- frontend-blog/pages/test-load.js
+++ frontend-blog/pages/test-load.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react'
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import Button from '@mui/material/Button';
-
+import ReactLoading from 'react-loading';
 
 export default function testload() {
-    const classes = useState();
     const [open, setOpen] = React.useState(false);
     const handleClose = () => {
         setOpen(false);
@@ -14,22 +13,22 @@ export default function testload() {
         setOpen(!open);
     };
 
-    const [post, setPost] = useState(null)
+    const [data, setData] = useState(null)
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts/1")
             .then(res => res.json())
             .then(res => {
                 const { body } = res
-                console.log(body)
-                setPost(body)
+                setData(body)
             })
+           
     }, [])
 
     return (
         <div>
             <h1>testload</h1>
 
-            {post ? post : 
+            {data ? data : 
             <Backdrop
                 sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                 open
