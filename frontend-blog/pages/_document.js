89578829e908994ea795a01e62f07beb8159efb2
honnage <hon_da-1998@hotmail.com>
2022-07-21 13:19:02 +0700

    test SpeedDial

--- frontend-blog/pages/_document.js
+++ frontend-blog/pages/_document.js
@@ -36,6 +36,7 @@ class MyDocument extends Document {
                     {/* <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" /> */}
 
                     <link href="assets/css/style.css" rel="stylesheet" />
+                    <link href="assets/css/custom.css" rel="stylesheet" />
 
                     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 
--- frontend-blog/public/assets/js/main.js
+++ frontend-blog/public/assets/js/main.js
@@ -107,6 +107,8 @@
   }
 
 
+
+
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
